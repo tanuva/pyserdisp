f30da4a0be6e88d552d3b1a1c8c35d6b94246c3b
Marcel Brueggebors <marcel@nightsoul.org>
2014-05-22 23:50:40 +0200

    Add text rendering through freetype

--- graphdisp.py
+++ graphdisp.py
@@ -1,9 +1,11 @@
 from pyserdisp import Serdisp
+from textrenderer import Font
 import Image
 
 class GraphDisp(Serdisp):
 	def __init__(self, device, model, options = ""):
 		self.disp = Serdisp.__init__(self, device, model, options)
+		self.font = Font("DroidSans.ttf", 12)
 
 	def __enter__(self):
 		Serdisp.__enter__(self)
@@ -12,7 +14,7 @@ class GraphDisp(Serdisp):
 	def __exit__(self, type, value, traceback):
 		Serdisp.__exit__(self, type, value, traceback)
 
-	def showPixmap(self, path):
+	def drawPixmap(self, path):
 		img = None
 		try:
 			img = Image.open(path)
@@ -30,3 +32,12 @@ class GraphDisp(Serdisp):
 			for y in range(height):
 				Serdisp.setColour(self, [x, y], bg[y][x])
 
+	def drawText(self, textpos, text, colour = Serdisp.BLACK):
+		bitmap = self.font.render_text(text)
+		for y in range(bitmap.height):
+			for x in range(bitmap.width):
+				pixpos = [textpos[0] + x, textpos[1] + y]
+				#print x, y, bitmap.pixels[x+y*bitmap.width]
+				pixBit = (1 - bitmap.pixels[x + y * bitmap.width])
+				#print pixBit, colour, pixCol, pixBit * colour[0]
+				Serdisp.setGrey(self, pixpos, pixBit * 255)
